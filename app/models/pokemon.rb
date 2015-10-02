a5aaae626821f4d02938e71e10b93beb0cc024f8
ahite89 <ahite89@gmail.com>
2015-10-01 23:55:38 -0400

    added number variable under pokemon class

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -6,6 +6,7 @@ class Pokemon < ActiveRecord::Base
    pokemon_parsed = JSON.parse(pokemon.to_json)
 
    name = pokemon_parsed.each { |row| row["name"] }
+   number = pokemon_parsed.each { |row| row[""] }
  end
 
  validates :name, presence: true
