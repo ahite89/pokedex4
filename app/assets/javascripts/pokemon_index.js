ce40bcde5d480ac16c9c1a5dcb07057ef066cc9c
ahite89 <ahite89@gmail.com>
2015-10-09 00:29:48 -0400

    switched from mouseover to hover technique

--- app/assets/javascripts/pokemon_index.js
+++ app/assets/javascripts/pokemon_index.js
@@ -1,13 +1,7 @@
 $(document).ready(function() {
-  $('#sprite img').mouseenter(function(){
-      $('#sprite').append("<div id='hoveringTooltip' style='position:fixed;'></div>");
-      $('#hoveringTooltip').html("Some pokemon");
-      $('#hoveringTooltip').css({
-          "top" : $(this).offset().top + MYTOPOFFSET,
-          "left" : $(this).offset().left + MYLEFTOFFSET
-      });
-  });
-  $('#sprite img').mouseleave(function(){
-      $('#hoveringTooltip').remove();
-  });
+    $('#sprite img').hover(function() {
+        $('#popup').show();
+    }, function() {
+        $('#popup').hide();
+    });
 });
