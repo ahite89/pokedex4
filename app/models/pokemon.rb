37939fb0ce37420f800c7c02cc01eb7ae0999cbc
ahite89 <ahite89@gmail.com>
2015-10-08 23:22:38 -0400

    deleted hash approach to ordering pokemon

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -8,38 +8,32 @@ class Pokemon < ActiveRecord::Base
    name = pokemon_parsed.each { |row| row["name"] }
    number = pokemon_parsed.each { |row| row["resource_uri"].split("/").last.to_i }
 
-   ordered = {}
-   pokemon_parsed.each do |row|
-   number = row["resource_uri"].split("/").last.to_i
-      ordered[row["name"].capitalize] = number
-    end
+   #stats_response = HTTParty.get("http://pokeapi.co/api/v1/pokemon/")
+   #stats = JSON.parse(stats_response.to_json)
 
-   stats_response = HTTParty.get("http://pokeapi.co/api/v1/pokemon/")
-   stats = JSON.parse(stats_response.to_json)
+   #hp = stats["hp"]
+   #attack = stats["attack"]
+   #defense = stats["defense"]
+   #sp_atk = stats["sp_atk"]
+   #sp_def = stats["sp_def"]
+   #speed = stats["speed"]
+   #height = stats["height"]
+   #weight = stats["weight"]
 
-   hp = stats.each { |row| row["hp"] }
-   attack = stats["attack"]
-   defense = stats["defense"]
-   sp_atk = stats["sp_atk"]
-   sp_def = stats["sp_def"]
-   speed = stats["speed"]
-   height = stats["height"]
-   weight = stats["weight"]
-
-   description_response = HTTParty.get("http://pokeapi.co/api/v1/description/")
-   description = JSON.parse(description_response.to_json)
+   #description_response = HTTParty.get("http://pokeapi.co/api/v1/description/")
+   #description = JSON.parse(description_response.to_json)
 
  end
 
  validates :name, presence: true
  validates :number, presence: true
- validates :hp, presence: true
- validates :attack, presence: true
- validates :defense, presence: true
- validates :sp_atk, presence: true
- validates :sp_def, presence: true
- validates :speed, presence: true
- validates :height, presence: true
- validates :weight, presence: true
+ #validates :hp, presence: true
+ #validates :attack, presence: true
+ #validates :defense, presence: true
+ #validates :sp_atk, presence: true
+ #validates :sp_def, presence: true
+ #validates :speed, presence: true
+ #validates :height, presence: true
+ #validates :weight, presence: true
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,9 @@
 #
 json = Pokemon.save_data_from_api
 pokemon_array = []
-json.each { |row| pokemon_array << [row["name"], row["resource_uri"].split("/").last.to_i, row["hp"]] }
+json.each { |row| pokemon_array << [row["name"], row["resource_uri"].split("/").last.to_i] }
 
-#ordered_pokemon = pokemon_hash.sort_by { |_k, v| v }
-pokemon_array.each { |name, number, hp| Pokemon.create(name: name, number: number, hp: hp) }
+
+ordered_pokemon = pokemon_hash.sort_by { |_k, v| v }
+pokemon_array.each { |name, number| Pokemon.create(name: name, number: number) }
+#Pokemon.create(hp: hp, attack: attack, defense: defense)
