c1ee6c3851c4bda800786a81c5b0d3a4e03ce34d
ahite89 <ahite89@gmail.com>
2015-10-08 23:57:47 -0400

    implemented mouseover technique

--- app/assets/javascripts/pokemon_index.js
+++ app/assets/javascripts/pokemon_index.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
-  $('#sprite img').hover(
-    function(){
-        $(this).addClass('bigger');
-    },
-    function(){
-        $(this).removeClass('bigger')
+  $('#sprite img').mouseenter(function(){
+      $('#sprite').append("<div id='hoveringTooltip' style='position:fixed;'></div>");
+      $('#hoveringTooltip').html("Some pokemon");
+      $('#hoveringTooltip').css({
+          "top" : $(this).offset().top + MYTOPOFFSET,
+          "left" : $(this).offset().left + MYLEFTOFFSET
+      });
+  });
+  $('#sprite img').mouseleave(function(){
+      $('#hoveringTooltip').remove();
   });
 });
